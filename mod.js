8efef9aa17fbc4e5fd65db5c8a2d05809580b767
wevefe <sirotinon.qwer123@gmail.com>
2026-06-16 01:40:07 +0300

    Create mod.js

---
+++ mod.js
@@ -0,0 +1,10 @@
+elements.space_sand = {
+    color: "#4a0e4e",                
+    behavior: behaviors.AGPOWDER,
+    category: "powders",
+    state: "solid",
+    density: 1200,
+    reactions: {
+        "water": { elem1: "stone", elem2: "ice" }
+    }
+};
