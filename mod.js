4ec20688c1ef9dac9837c711b1938380cc76ac7d
wevefe <sirotinon.qwer123@gmail.com>
2026-06-16 19:12:03 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,19 +1,10 @@
-// Ждем, пока Sandboxels полностью загрузит все свои внутренние скрипты
-window.addEventListener('load', function() {
-    // Проверяем, существуют ли базовые объекты игры
-    if (typeof elements !== 'undefined' && typeof behaviors !== 'undefined') {
-        
-        elements.wine = {
-            color: "#89281D",
-            behavior: behaviors.LIQUID,
-            category: "food",
-            viscosity: 20000,
-            state: "liquid",
-            density: 380,
-        };
-        
-        console.log("Мод на вино успешно активирован!");
-    } else {
-        console.error("Ошибка: База данных Sandboxels еще не готова.");
-    }
+runAfterLoad(function() {
+    elements.wine = {
+        color: "#89281D",
+        behavior: behaviors.LIQUID,
+        category: "food",
+        viscosity: 20000,
+        state: "liquid",
+        density: 380,
+    };
 });
