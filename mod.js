397f51e99944165a5f2722e80fa73db996efcc85
wevefe <sirotinon.qwer123@gmail.com>
2026-06-16 19:51:01 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,23 +1,16 @@
-// Функция безопасной интеграции мода
-function injectWineMod() {
-    // Проверяем, созданы ли уже базовые структуры движка Sandboxels
-    if (typeof elements !== 'undefined' && typeof behaviors !== 'undefined') {
-        
-        elements.wine = {
+function forceAddWine() {
+    if (window.elements) {
+        window.elements.wine = {
             color: "#89281D",
-            behavior: behaviors.LIQUID,
+            behavior: ["XX", "M2", "XX"], // Физика жидкости (течет вниз и вбок)
             category: "food",
             viscosity: 20000,
             state: "liquid",
             density: 380,
         };
-        
-        console.log("Мод на вино успешно загружен на neal.fun!");
+        console.log("Wine added successfully!");
     } else {
-        // Если игра еще загружается, делаем паузу в 100мс и пробуем снова
-        setTimeout(injectWineMod, 100);
+        setTimeout(forceAddWine, 200);
     }
 }
-
-// Запускаем проверку
-injectWineMod();
+forceAddWine();
