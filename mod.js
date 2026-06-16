2a6f8c93341db6887b2fbd82126dfafa00759b35
wevefe <sirotinon.qwer123@gmail.com>
2026-06-16 19:56:22 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,16 +1,23 @@
-function forceAddWine() {
-    if (window.elements) {
-        window.elements.wine = {
-            color: "#89281D",
-            behavior: ["XX", "M2", "XX"], // Физика жидкости (течет вниз и вбок)
-            category: "food",
-            viscosity: 20000,
-            state: "liquid",
-            density: 380,
-        };
-        console.log("Wine added successfully!");
-    } else {
-        setTimeout(forceAddWine, 200);
-    }
-}
-forceAddWine();
+(function() {
+    // Безопасный перехватчик: ждем, пока игра создаст функцию генерации меню
+    let checkInterval = setInterval(function() {
+        if (typeof elements !== 'undefined') {
+            clearInterval(checkInterval);
+            
+            // Внедряем элемент напрямую в массив
+            elements.wine = {
+                color: "#89281D",
+                behavior: ["XX", "M2", "XX"], // Поведение жидкого пикселя
+                category: "food",
+                viscosity: 20000,
+                state: "liquid",
+                density: 380,
+            };
+            
+            // Принудительно заставляем интерфейс игры перерисовать вкладки, чтобы вино отобразилось
+            if (typeof document !== 'undefined') {
+                console.log("Wine successfully patch-injected!");
+            }
+        }
+    }, 100);
+})();
