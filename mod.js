deb4c1d0dbeb12acd33069808376fa28744b1431
wevefe <sirotinon.qwer123@gmail.com>
2026-06-16 19:15:16 +0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,10 +1,8 @@
-runAfterLoad(function() {
-    elements.wine = {
-        color: "#89281D",
-        behavior: behaviors.LIQUID,
-        category: "food",
-        viscosity: 20000,
-        state: "liquid",
-        density: 380,
-    };
-});
+elements.wine = {
+    color: "#89281D",
+    behavior: behaviors.LIQUID,
+    category: "food",
+    viscosity: 20000,
+    state: "liquid",
+    density: 380,
+};
